768a4db61e6438dff3093d06dc3972819fcb7b74
Helen Nguyen <helennguyen30687@gmail.com>
2020-09-12 13:31:52 +0700

    add new repository

---
+++ selenium/javaForTester/Topic_01_Path.java
@@ -0,0 +1,10 @@
+package javaForTester;
+
+public class Topic_01_Path {
+
+	public static void main(String[] args) {
+		String ProjectFolder = System.getProperty("user.dir");
+		System.out.println(ProjectFolder);
+	}
+
+}
